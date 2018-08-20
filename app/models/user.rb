638da3b9ca13eb5117dcf03f29577e0ad3e9088f
Alex Marichal <marichalalex@yahoo.fr>
2018-08-20 16:40:54 +0200

    deleted somking

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :smokings, dependent: :destroy
+  has_many :suits, dependent: :destroy
 end
