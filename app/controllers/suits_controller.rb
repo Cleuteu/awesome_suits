fb76d8fbee6f7aaa12b27fd9750cfd0b4a8c57aa
Eugene Ernoult <e.ernoult@gmail.com>
2018-08-20 17:55:09 +0200

    New/create action

--- app/controllers/suits_controller.rb
+++ app/controllers/suits_controller.rb
@@ -1,2 +1,22 @@
 class SuitsController < ApplicationController
+  def new
+    @suit = Suit.new
+  end
+
+  def create
+    @suit = Suit.new(suit_params)
+    @suit.user_id = current_user
+
+    if @suit.save
+      redirect_to suit_path(@suit)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def suit_params
+    params.require(:suit).permit(:user_id, :color, :description, :style, :size)
+  end
 end
