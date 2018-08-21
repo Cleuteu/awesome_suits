59f7b11444673ac93ebc9641d0da1e6138df72be
Alex Marichal <marichalalex@yahoo.fr>
2018-08-21 12:45:18 +0200

    add names to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ Suit.destroy_all
 puts 'Creating users and suits...'
 10.times do
   user = User.new(email: Faker::Internet.email,
-              password: Faker::Number.number(7))
+              password: Faker::Number.number(7),
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name)
   user.save!
   suit = Suit.new(name: Faker::Pokemon.name,
                 color: Faker::Color.color_name,
@@ -16,13 +18,9 @@ puts 'Creating users and suits...'
 end
 
 puts 'Creating users Thomas, Eugene, Yoann and Alex'
-User.create!(email: 'marichalalex@yahoo.fr',
-              password: 'azerty')
-User.create!(email: 'e.ernoult@gmail.com',
-              password: 'azerty')
-User.create!(email: 'thomas.pouillevet@gmail.com',
-              password: 'azerty')
-User.create!(email: 'yoann.saunier@gmail.com',
-              password: 'azerty')
+User.create!(email: 'marichalalex@yahoo.fr', first_name: 'Alex', last_name: 'Marichal', password: 'azerty')
+User.create!(email: 'e.ernoult@gmail.com', first_name: 'Eugene', last_name: 'Ernoult', password: 'azerty')
+User.create!(email: 'thomas.pouillevet@gmail.com', first_name: 'Thomas', last_name: 'Pouillevet', password: 'azerty')
+User.create!(email: 'yoann.saunier@gmail.com', first_name: 'Yoann', last_name: 'Saunier', password: 'azerty')
 
 puts 'Finished!'
