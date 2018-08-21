383295e32dc658bac65340df6d1346530c664906
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2018-08-21 15:10:17 +0200

    fix navbar and typed.js ne pas oublier bundle install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,6 @@ import "bootstrap";
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
