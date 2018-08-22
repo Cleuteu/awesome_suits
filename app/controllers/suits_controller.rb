9e1885842ad94d3d2704fd6e730cc0c091bde464
Alex Marichal <marichalalex@yahoo.fr>
2018-08-22 13:29:44 +0200

    bug with map fixed

--- app/controllers/suits_controller.rb
+++ app/controllers/suits_controller.rb
@@ -25,12 +25,12 @@ class SuitsController < ApplicationController
       }
     end
     authorize @suit
-    @marker =
+    @markers =[
       {
         lat: @suit.latitude,
         lng: @suit.longitude#,
         # infoWindow: { content: render_to_string(partial: "/suits/map_box", locals: { suit: suit }) }
-      }
+      }]
   end
 
   def new
