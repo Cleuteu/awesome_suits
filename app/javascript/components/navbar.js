1d0984f56e24ae3cde2bf7ddadda486a7b3b2471
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2018-08-23 13:13:16 +0200

    landing page et navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,28 +1,28 @@
-function initUpdateNavbarOnScroll() {
-  const navbar = document.querySelector('.navbar-wagon');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-wagon-black');
-      } else {
-        navbar.classList.remove('navbar-wagon-black');
-      }
-    });
-  }
-}
+// function initUpdateNavbarOnScroll() {
+//   const navbar = document.querySelector('.navbar-wagon');
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight) {
+//         navbar.classList.add('navbar-wagon-black');
+//       } else {
+//         navbar.classList.remove('navbar-wagon-black');
+//       }
+//     });
+//   }
+// }
 
-function changeNavbarBackOnHomepage() {
-  const path = window.location.pathname
-  const navbar = document.querySelector('.navbar-wagon');
-  const banner = document.querySelector('.banner');
-  if (path === "/") {
-    navbar.classList.add('navbar-wagon-background-transparent');
-    banner.classList.add('banner-under-navbar');
-  }
-  else if (path === "/suits/new") {
-    navbar.classList.add('navbar-wagon-background-transparent');
-  }
-}
+// function changeNavbarBackOnHomepage() {
+//   const path = window.location.pathname
+//   const navbar = document.querySelector('.navbar-wagon');
+//   const banner = document.querySelector('.banner');
+//   if (path === "/") {
+//     navbar.classList.add('navbar-wagon-background-transparent');
+//     banner.classList.add('banner-under-navbar');
+//   }
+//   else if (path === "/suits/new") {
+//     navbar.classList.add('navbar-wagon-background-transparent');
+//   }
+// }
 
-export { initUpdateNavbarOnScroll };
-export { changeNavbarBackOnHomepage };
+// export { initUpdateNavbarOnScroll };
+// export { changeNavbarBackOnHomepage };
