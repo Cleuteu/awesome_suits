c466d825d99796fc1399970bb2471aafd4d02791
Eugene Ernoult <e.ernoult@gmail.com>
2018-08-23 11:42:38 +0200

    Datepicker with range

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,14 +1,17 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 const rentingForm = document.getElementById('renting-form-div');
 const rentings = JSON.parse(rentingForm.dataset.rentings);
 
 if (rentingForm) {
-  flatpickr(".datepicker", {
+  flatpickr("#renting_start_date", {
+    mode: "range",
     minDate: "today",
     inline: true,
     dateFormat: "Y-m-d",
+    plugins: [new rangePlugin({ input: "#renting_end_date"})],
     "disable": rentings,
   })
 }
