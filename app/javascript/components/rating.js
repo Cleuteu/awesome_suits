bd23e702306ca9221b7bf8ea7b3f5d68cac85637
Eugene <40290497+e-ernoult@users.noreply.github.com>
2018-08-24 15:50:23 +0200

    Update rating.js

--- app/javascript/components/rating.js
+++ app/javascript/components/rating.js
@@ -1,7 +1,4 @@
 document.querySelectorAll("star-rate").forEach((star) => {
-  // star.addEventListener("click", (event) => {
-  //   const note = event.currentTarget.value;
-  console.log('Vu')
-  // });
+
 });
 
